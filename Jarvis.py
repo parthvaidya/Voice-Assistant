e8ed71d94165f5568ac73b26e11bd6e3cf13753f
parthvaidya <47813732+parthvaidya@users.noreply.github.com>
2020-03-30 22:03:45 +0530

    Create Jarvis.py

---
+++ Jarvis.py
@@ -0,0 +1,81 @@
+import pyttsx3 
+import speech_recognition as sr 
+import datetime
+import wikipedia
+import webbrowser
+import os
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("Hello sir my name is Jarvis. Please tell me how may I help you")     
+
+def takeCommand():
+    
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+            
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+
+
+if __name__ == "__main__":
+    wishMe()
+    #while True:
+    if 1:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak ('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open('youtube.com')
+        
+        elif 'open google' in query:
+            webbrowser.open('google.com')
+
+        elif 'open facebook' in query:
+            webbrowser.open('facebook.com')
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        
